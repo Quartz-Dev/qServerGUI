f6e0ab195b2b8dbc6d91bae83c4bf166124bd158
QarthO <qartho@gmail.com>
2021-04-19 13:10:55 -0400

    added cpu/ram/disk monitoring

--- src/js/performance.js
+++ src/js/performance.js
@@ -1,8 +1,9 @@
 var osu = require('node-os-utils')
+var disk = require('check-disk-space')
 var cpu = osu.cpu
 var mem = osu.mem
-var drive = osu.drive
 var netstat = osu.netstat
+
 
 getCPUUsage()
 
@@ -22,10 +23,37 @@ function getMemUsage(){
         var used = info.usedMemMb
         var total = info.totalMemMb
         var usage = Math.round((used/total)*100)
-        console.log("RAM: " + used + "/" + total)
-        console.log("RAM Usage: " + usage)
         $('#ram-usage-progress').val(usage)
         $('#ram-usage-percent').html("RAM Usage: " + usage + "% ")
         getMemUsage()
     })
+}
+
+getDiskSpace()
+
+
+var config = require('electron-json-config');
+
+function getDiskSpace(){
+    var path = config.get('server-dir')
+    disk(path).then((diskSpace) => {
+        var size = diskSpace.size
+        var free = diskSpace.free
+        var used = Math.round((1 - (free/size))*100)
+        $('#disk-space-progress').val(used)
+        $('#disk-space-percent').html("Disk Space: " + used + "% ")
+        getDiskSpace()
+    })
+}
+
+function startUptimeTimer(){
+    
+}
+
+function test(){
+    var foo = process.memoryUsage().heapUsed / 1024 / 1024
+    var foo2 = process.getSystemMemoryInfo().total / 1024
+    console.log("Used:" + Math.round(foo * 100) / 100 )
+    console.log("Total:" + Math.round(foo2 * 100) / 100 )
+    console.log("Percentage:" + (foo/foo2*100).toFixed(2) + "%")
 }
